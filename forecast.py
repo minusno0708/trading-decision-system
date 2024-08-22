d7162c053151090f309fa6e81396f3756998a598
minusno0708 <naoyao1014@gmail.com>
2024-08-22 14:56:14 +0900

    値を標準化

--- forecast.py
+++ forecast.py
@@ -1,8 +1,10 @@
 import pandas as pd
 
+from sklearn.preprocessing import StandardScaler
+
 data_path = "dataset/btc.csv"
 
-def data_loader(file_path: str) -> pd.DataFrame:
+def data_loader(file_path: str) -> [pd.DataFrame, pd.DataFrame]:
     df_row = pd.read_csv(file_path)
 
     # 不要な列を削除
@@ -14,9 +16,23 @@ def data_loader(file_path: str) -> pd.DataFrame:
 
     # 日付順にソート
     df_row = df_row.sort_values("timeOpen")
+    df_row = df_row.set_index("timeOpen")
 
-    return df_row
+    # 値を正規化
+    scaler = StandardScaler()
+    df_row["close"] = scaler.fit_transform(df_row["close"].values.reshape(-1, 1))
+
+    # データを分割
+    rate = 0.8
+    n_train = int(len(df_row) * rate)
+    n_test = len(df_row) - n_train
+
+    train_data = df_row.iloc[:n_train]
+    test_data = df_row.iloc[n_train:]
+
+    return train_data, test_data
 
 if __name__ == "__main__":
-    data = data_loader(data_path)
-    print(data.head())
+    train, test = data_loader(data_path)
+    print(train.head())
+    print(test.head())
