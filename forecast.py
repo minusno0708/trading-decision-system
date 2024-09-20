cef243c11d6b6d9471a08d78da1dfa09fc2fd281
minusno0708 <naoyao1014@gmail.com>
2024-09-20 18:11:39 +0900

    グラフを描画

--- forecast.py
+++ forecast.py
@@ -9,17 +9,12 @@ from gluonts.torch import DeepAREstimator
 
 data_path = "dataset/btc.csv"
 
-prediction_length = 36
+input_length = 30
+output_length = 7
 
-def do_deepar(data: pd.DataFrame):
-    data = ListDataset(
-        [
-            {
-                "start": data.index[0],
-                "target": data.close[:data.index[0]]
-            }
-        ]
-    )
+def draw_graph(x_data: list, y_data: list, name: str):
+    plt.plot(x_data, y_data)
+    plt.savefig(f"output/{name}.png")
 
 def data_loader(file_path: str) -> [pd.DataFrame, pd.DataFrame]:
     df_row = pd.read_csv(file_path)
@@ -43,16 +38,33 @@ def data_loader(file_path: str) -> [pd.DataFrame, pd.DataFrame]:
     rate = 0.8
     n_train = int(len(df_row) * rate)
 
-    train_data = df_row.iloc[:n_train + prediction_length]
+    train_data = df_row.iloc[:n_train + output_length]
     test_data = df_row.iloc[n_train:]
 
     return train_data, test_data
 
+def train_model(train_data: pd.DataFrame) -> DeepAREstimator:
+    return DeepAREstimator(
+        context_length=input_length,
+        prediction_length=output_length,
+        freq="D",
+        trainer_kwargs={"max_epochs": 5}
+    ).train(PandasDataset(train_data, target="close"))
+    
+
 if __name__ == "__main__":
     train_data, test_data = data_loader(data_path)
 
+    draw_graph(list(train_data.index), list(train_data["close"]), "train_data")
+
+    print(train_data)
+    print(type(list(train_data.index)))
+
+    """
+
     model = DeepAREstimator(
-        prediction_length=prediction_length,
+        context_length=input_length,
+        prediction_length=output_length,
         freq="D",
         trainer_kwargs={"max_epochs": 5}
     ).train(PandasDataset(train_data, target="close"))
@@ -63,3 +75,4 @@ if __name__ == "__main__":
     for forecast in forecasts:
         forecast.plot()
     plt.legend(["True values"], loc="upper left", fontsize="xx-large")
+    """
