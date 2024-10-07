32dd34575127b9a2040dc870f7080f1ca5b82317
minusno0708 <naoyao1014@gmail.com>
2024-10-07 22:02:12 +0900

    chore: simulationディレクトリを作成

--- simulation/have_assets.py
+++ simulation/have_assets.py
@@ -2,21 +2,24 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-from data_provider.data_loader import DataLoader
 import matplotlib.dates as mdates
 
 import torch
+
+import os
+import sys
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+
+from data_provider.data_loader import DataLoader
 from model.DeepAR import Model
-
-
 from strutegy import *
 
 
 input_length = 30
-output_length = 7
+output_length = 1
 
 trade_rate = 0.1
-start_yen = 100000
+start_dollar = 1000
 
 seed = 0
 
@@ -24,8 +27,11 @@ random.seed(seed)
 np.random.seed(seed)
 torch.manual_seed(seed)
 
-train_flag = True
+decision_method = "all_lose"
 
+train_flag = False
+
+output_dir = "output"
 
 class Log:
     def __init__(self):
@@ -45,7 +51,7 @@ class Log:
 
         if title:
             plt.title(title)
-        plt.savefig(f"output/images/{name}.png")
+        plt.savefig(f"{output_dir}/images/{name}.png")
         plt.clf()
         plt.close()
 
@@ -92,11 +98,18 @@ class TradeRate:
         return self.rates[self.target_assets.index(target_asset)]
 
 def main():
-    base_asset = "yen"
+    actions_result = {
+        "buy_win": 0,
+        "buy_lose": 0,
+        "sell_win": 0,
+        "sell_lose": 0,
+    }
+
+    base_asset = "dollar"
     target_assets = ["btc"]
 
     assets = Assets([base_asset] + target_assets)
-    assets.get(base_asset, start_yen)
+    assets.get(base_asset, start_dollar)
 
     rate = TradeRate(base_asset, target_assets)
 
@@ -107,12 +120,14 @@ def main():
     model = Model(input_length, output_length)
     if train_flag:
         model.train(train_data)
+        model.save(f"{output_dir}/models/{target_assets[0]}_model")
     else:
-        model.load(f"output/models/{target_assets[0]}_model")
+        model.load(f"{output_dir}/models/{target_assets[0]}_model")
 
     rate_log = Log()
-    yen_log = Log()
+    dollar_log = Log()
     crypto_log = Log()
+    total_log = Log()
 
     for d in range(0, len(raw_data) - input_length - output_length):
         target_scaled_data = test_data.iloc[range(d, input_length + d), [0]]
@@ -131,37 +146,69 @@ def main():
         history_values = data_loader.inverse_transform(target_scaled_data.values.reshape(-1))[0]
         forecast_values = data_loader.inverse_transform(forecasts[0].mean)[0]
 
-        #action = diff_next_mean(history_values, forecast_values)
-        action = random_decision()
-        #action = all_lose(history_values, tommorow_data.values[0][0])
+        if decision_method == "diff_next_mean":
+            action = diff_next_mean(history_values, forecast_values)
+        elif decision_method == "random":
+            action = random_decision()
+        elif decision_method == "all_win":
+            action = all_win(history_values, tommorow_data.values[0][0])
+        elif decision_method == "all_lose":
+            action = all_lose(history_values, tommorow_data.values[0][0])
+        elif decision_method == "all_buy":
+            action = "buy"
+        elif decision_method == "all_sell":
+            action = "sell"
+        elif decision_method == "cross_action":
+            if not "action" in locals():
+                action = "sell"
+            elif action == "buy":
+                action = "sell"
+            elif action == "sell":
+                action = "buy"
+
+        current_date = target_raw_data.index[-1]
 
         if action == "buy":
-            assets.trade("yen", "btc", rate.buy("btc"), assets.yen.possession * trade_rate)
+            assets.trade("dollar", "btc", rate.buy("btc"), assets.dollar.possession * trade_rate)
         elif action == "sell":
-            assets.trade("btc", "yen", rate.sell("btc"), assets.btc.possession * trade_rate)
+            assets.trade("btc", "dollar", rate.sell("btc"), assets.btc.possession * trade_rate)
         else:
             pass
 
-        #print(f"action: {action}, current: {history_values[-1]}, forecast: {forecast_values[0]}")
-
-        # 結果を出力
-        current_date = target_raw_data.index[-1]
-        #print(f"date: {current_date}, action: {action}, result: yen {assets.yen.possession}, btc {assets.btc.possession}")
+        if action == "buy":
+            if history_values[-1] < tommorow_data.values[0][0]:
+                actions_result["buy_win"] += 1
+            elif history_values[-1] > tommorow_data.values[0][0]:
+                actions_result["buy_lose"] += 1
+            else:
+                print("Error")
+        elif action == "sell":
+            if history_values[-1] > tommorow_data.values[0][0]:
+                actions_result["sell_win"] += 1
+            elif history_values[-1] < tommorow_data.values[0][0]:
+                actions_result["sell_lose"] += 1
+            else:
+                print("Error")
 
         # ログを保存
         rate_log.append(current_date, rate.rates[0])
-        yen_log.append(current_date, assets.yen.possession)
+        dollar_log.append(current_date, assets.dollar.possession)
         crypto_log.append(current_date, assets.btc.possession)
+        total_log.append(current_date, assets.dollar.possession + assets.btc.possession * rate.rates[0])
         
 
-    rate_log.plot(f"{target_assets[0]}-rate", "BTC/JPY Rate")
-    yen_log.plot(f"{target_assets[0]}-yen", "Yen Possession")
-    crypto_log.plot(f"{target_assets[0]}-{target_assets[0]}", "BTC Possession")
+    rate_log.plot(f"{target_assets[0]}-rate", "btc/USD Rate")
+    dollar_log.plot(f"{target_assets[0]}-dollar", "dollar Possession")
+    crypto_log.plot(f"{target_assets[0]}-{target_assets[0]}", "btc Possession")
+    total_log.plot(f"{target_assets[0]}-total", "Total Possession")
 
-    assets.trade(f"{target_assets[0]}", "yen", rate.sell(f"{target_assets[0]}"), assets.btc.possession)
+    assets.trade(f"{target_assets[0]}", "dollar", rate.sell(f"{target_assets[0]}"), assets.btc.possession)
 
-    print(f"result: yen {assets.yen.possession}, {target_assets[0]} {assets.btc.possession}")
+    print(f"result: dollar {assets.dollar.possession}, {target_assets[0]} {assets.btc.possession}")
+
+    print(f"actions_result: {actions_result}")
 
 if __name__ == '__main__':
+    print("desicion method: ", decision_method)
     main()
     
