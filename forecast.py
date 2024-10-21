7dc4c0a23f3a29854aa9a1faee1a4826f9475d72
minusno0708 <naoyao1014@gmail.com>
2024-10-21 18:00:39 +0900

    確率予測の描画の内容を変更

--- forecast.py
+++ forecast.py
@@ -2,8 +2,8 @@ import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 
 from data_provider.data_loader import DataLoader
-#from model.DeepAR import TorchModel as Model
-from model.DeepAR import MxModel as Model
+from model.DeepAR import TorchModel as Model
+#from model.DeepAR import MxModel as Model
 
 import numpy as np
 import pandas as pd
@@ -13,7 +13,7 @@ import mxnet as mx
 import datetime
 
 input_length = 30
-output_length = 30
+output_length = 7
 
 train_flag = True
 
@@ -30,43 +30,41 @@ def draw_graph(x_data: list, y_data: list, name: str):
     plt.clf()
     plt.close()
 
-def draw_predict_graph(input_data: pd.DataFrame, forecasts: list, correct_data: pd.DataFrame, crypto: str):
+def draw_predict_graph(input_data: pd.DataFrame, forecasts: list, correct_data: pd.DataFrame, crypto: str, num: int):
     fig, ax = plt.subplots()
 
     ax.plot(input_data.index, input_data.values, label="input")
     ax.plot(correct_data.index, correct_data.values, label="correct")
     
 
-    forecasts[0].plot()
+    forecasts[0].plot(intervals=(0.3, 0.5))
     ax.plot(forecasts[0].index, forecasts[0].median, label="forecast-median")
     ax.plot(forecasts[0].index, forecasts[0].mean, label="forecast-mean")
 
-    ax.xaxis.set_major_locator(mdates.DayLocator(interval=10))
+    ax.xaxis.set_major_locator(mdates.DayLocator(interval=20))
     ax.xaxis.set_major_formatter(mdates.DateFormatter("%Y-%m-%d"))
 
     plt.legend()
     plt.xlabel("Date")
-    plt.savefig(f"output/images/{crypto}-forecast.png")
+    plt.savefig(f"output/images/forecast/{num}-{crypto}-forecast.png")
 
 def rmse(y_true, y_pred):
-    return np.sqrt(np.mean((y_true - y_pred) ** 2))
+    return np.sqrt((y_true - y_pred) ** 2)
 
 def mae(y_true, y_pred):
     return np.mean(np.abs(y_true - y_pred))
 
-if __name__ == "__main__":
+def main(train_start_year: int = 2010, test_start_year: int = 2023):
     crypto = "btc"
 
-    data_loader = DataLoader(output_length)
-    train_data, test_data = data_loader.load(f"{crypto}.csv", True, datetime.datetime(2018, 1, 1), datetime.datetime(2023, 1, 1))
+    data_loader = DataLoader(input_length)
+    train_data, test_data = data_loader.load(f"{crypto}.csv", True, datetime.datetime(train_start_year, 1, 1), datetime.datetime(test_start_year, 1, 1))
 
-    mean_loss = np.array([])
-    median_loss = np.array([])
-
+    mean_loss = np.empty((0, output_length))
     mean_correct = np.array([])
-    median_correct = np.array([])
 
-    updown_trend = np.array([0, 0])
+    forecast_updown = np.array([])
+    correct_updown = np.array([])
 
     model = Model(input_length, output_length)
 
@@ -82,36 +80,48 @@ if __name__ == "__main__":
         correct_data = test_data.iloc[range(i + input_length, input_length + i + output_length), [0]]
 
         forecasts = model.forecast(target_data)
+        if i % 100 == 0:
+            draw_predict_graph(target_data, forecasts, correct_data, crypto, i)
 
-        correct_inverse = data_loader.inverse_transform(correct_data.values.flatten())[0][0]
-        mean_inverse = data_loader.inverse_transform(forecasts[0].mean)[0][0]
-        median_inverse = data_loader.inverse_transform(forecasts[0].median)[0][0]
+        correct_inverse = data_loader.inverse_transform(correct_data.values.flatten())[0]
+        mean_inverse = data_loader.inverse_transform(forecasts[0].mean)[0]
 
-        mean_loss = np.append(mean_loss, rmse(correct_inverse, mean_inverse))
-        median_loss = np.append(median_loss, rmse(correct_inverse, median_inverse))
-        if correct_inverse < mean_inverse:
-            updown_trend[0] += 1
-        else:
-            updown_trend[1] += 1
+        mean_loss = np.append(mean_loss, [rmse(correct_inverse, mean_inverse)], axis=0)
 
         correct_flag = target_data.values[-1] < correct_data.values.flatten()[0]
         mean_flag = target_data.values[-1] < forecasts[0].mean[0]
-        median_flag = target_data.values[-1] < forecasts[0].median[0]
 
-        if correct_flag == mean_flag:
-            mean_correct = np.append(mean_correct, 1)
-        else:
-            mean_correct = np.append(mean_correct, 0)
+        correct_updown = np.append(correct_updown, correct_flag)
+        forecast_updown = np.append(forecast_updown, mean_flag)
 
-        if correct_flag == median_flag:
-            median_correct = np.append(median_correct, 1)
-        else:
-            median_correct = np.append(median_correct, 0)
+        mean_correct = np.append(mean_correct, correct_flag == mean_flag)
+    
+    """
+    with open("test.txt", "a") as f:
+        f.write(f"Train: {train_start_year}, Test: {test_start_year}\n")
         
-    print(f"UpDown: {updown_trend}")
-    print(f"MeanLoss: {mean_loss.mean()}")
-    print(f"MeanCorrect: {mean_correct.mean()}")
-    print(f"MedianLoss: {median_loss.mean()}")
-    print(f"MedianCorrect: {median_correct.mean()}")
+        f.write(f"MeanLoss: {mean_loss.mean(axis=0)}\n")
+        f.write(f"MeanCorrect: {mean_correct.mean()}\n")
+        f.write(f"CorrectUp Rate: {correct_updown.mean()}\n")
+        f.write(f"ForecastUp Rate: {forecast_updown.mean()}\n")
+
+        f.write("\n\n")
+    """
+    print(f"MeanLoss: {mean_loss.mean(axis=0)}\n")
+    print(f"MeanCorrect: {mean_correct.mean()}\n")
+    print(f"CorrectUp Rate: {correct_updown.mean()}\n")
+    print(f"ForecastUp Rate: {forecast_updown.mean()}\n")
 
     print("success")
+
+
+if __name__ == "__main__":
+    main()
+    """
+    year_term = [2010, 2024]
+    train_year = 2
+    test_year = 1
+    for i in range(year_term[0], year_term[1] - test_year):
+        print(f"Train: {i}, Test: {i + train_year}")
+        main(i, i + train_year)
+    """
