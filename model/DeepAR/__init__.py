59179d2b630c34e92b1314785ef972a205e895ed
minusno0708 <naoyao1014@gmail.com>
2024-11-07 03:51:36 +0900

    学習モデルの機能をinitに統一

--- model/DeepAR/__init__.py
+++ model/DeepAR/__init__.py
@@ -1,7 +1,16 @@
 import pandas as pd
 
-from model.DeepAR.torch import Model as TorchModel
-from model.DeepAR.mxnet import Model as MxModel
+from gluonts.dataset.common import ListDataset
+from gluonts.evaluation.backtest import make_evaluation_predictions, backtest_metrics
+from gluonts.model.predictor import Predictor
+
+from gluonts.evaluation import Evaluator
+
+from pathlib import Path
+import os
+
+from model.DeepAR.torch import model as torch_model
+from model.DeepAR.mxnet import model as mx_model
 
 class Model:
     def __init__(self, input_length: int, output_length: int, model_type="torch"):
@@ -9,27 +18,72 @@ class Model:
         self.output_length = output_length
 
         if model_type == "torch":
-            self.model = TorchModel(input_length, output_length)
+            self.model = torch_model(input_length, output_length)
         elif model_type == "mxnet":
-            self.model = MxModel(input_length, output_length)
+            self.model = mx_model(input_length, output_length)
 
     def train(self, train_data: pd.DataFrame):
-        self.model.train(train_data)
+        dataset = ListDataset(
+            [{"start": train_data.index[0], "target": train_data["close"]}],
+            freq="1D",
+        )
+
+        self.model = self.model.train(dataset)
 
     def forecast(self, test_data: pd.DataFrame):
-        return self.model.forecast(test_data)
+        dataset = ListDataset(
+            [{"start": test_data.index[0], "target": test_data["close"]}],
+            freq="1D",
+        )
+
+        forecasts = list(self.model.predict(dataset))
+
+        return forecasts
 
     # テストデータの末尾の部分を予測した結果の評価
     def evaluate(self, test_data: pd.DataFrame):
-        return self.model.evaluate(test_data)
+        dataset = ListDataset(
+            [{"start": test_data.index[0], "target": test_data["close"]}],
+            freq="1D",
+        )
+
+        forecast_it, ts_it = make_evaluation_predictions(
+            dataset=dataset,
+            predictor=self.model,
+            num_samples=1000
+        )
+
+        evaluator = Evaluator()
+        agg_metrics, item_metrics = evaluator(ts_it, forecast_it, num_series=len(dataset))
+       
+        return agg_metrics, item_metrics
 
     # テストデータ全体の評価
     def backtest(self, test_data: pd.DataFrame):
-        return self.model.backtest(test_data)
+        evaluator = Evaluator(quantiles=[0.1, 0.5, 0.9])
+
+        test_dataset = ListDataset(
+            [{"start": test_data.index[0], "target": test_data["close"]}],
+            freq="1D",
+        )
+
+        agg_metrics, item_metrics = backtest_metrics(
+            test_dataset=test_dataset,
+            predictor=self.model,
+            evaluator=evaluator,
+        )
+
+        return agg_metrics, item_metrics
 
     def save(self, str_path: str):
-        self.model.save(str_path)
+        path = Path(str_path)
+
+        if not os.path.exists(path):
+            os.makedirs(path)
+
+        self.model.serialize(path)
 
     def load(self, str_path: str):
-        self.model.load(str_path)
+        path = Path(str_path)
+        self.model = Predictor.deserialize(path)
 
--- model/DeepAR/mxnet.py
+++ model/DeepAR/mxnet.py
@@ -1,59 +1,17 @@
 import pandas as pd
 
-import torch
-import torch.nn as nn
-
-from gluonts.dataset.common import ListDataset
 from gluonts.mx.model.deepar import DeepAREstimator as MxDeepAREstimator
-from gluonts.evaluation.backtest import make_evaluation_predictions
-from gluonts.model.predictor import Predictor
 from gluonts.mx.trainer import Trainer
 
-from pathlib import Path
-import os
 
-class Model:
-    def __init__(self, input_length: int, output_length: int):
-        self.input_length = input_length
-        self.output_length = output_length
+def model(input_length: int, output_length: int):
+    trainer = Trainer(
+        epochs=50,
+    )
 
-        trainer = Trainer(
-            epochs=50,
-        )
-
-        self.model = MxDeepAREstimator(
-            prediction_length=output_length,
-            context_length=input_length,
-            trainer=trainer,
-            freq="D",
-        )
-
-    def train(self, train_data: pd.DataFrame):
-        dataset = ListDataset(
-            [{"start": train_data.index[0], "target": train_data["close"]}],
-            freq="D",
-        )
-
-        self.model = self.model.train(dataset)
-
-    def forecast(self, test_data: pd.DataFrame):
-        dataset = ListDataset(
-            [{"start": test_data.index[0], "target": test_data["close"]}],
-            freq="D",
-        )
-
-        forecasts = list(self.model.predict(dataset))
-
-        return forecasts
-
-    def save(self, str_path: str):
-        path = Path(str_path)
-
-        if not os.path.exists(path):
-            os.makedirs(path)
-
-        self.model.serialize(path)
-
-    def load(self, str_path: str):
-        path = Path(str_path)
-        self.model = Predictor.deserialize(path)
+    return MxDeepAREstimator(
+        prediction_length=output_length,
+        context_length=input_length,
+        trainer=trainer,
+        freq="D",
+    )
