2288c988b11dbd8ac16df53d313b9b9d33197154
minusno0708 <naoyao1014@gmail.com>
2024-12-02 11:26:43 +0900

    予測時にサンプルパスを生成

--- model/pytorch/output.py
+++ model/pytorch/output.py
@@ -3,12 +3,43 @@ import numpy as np
 class ForecastOutput:
     def __init__(self, mean, var, num_samples = 100):
         self.distribution = {
-            "mean": mean,
-            "var": var
+            "mean": mean[0, :, :],
+            "var": var[0, :, :]
         }
+        self.num_data = mean.shape[0]
+        self.prediction_length = mean.shape[1]
         self.num_samples = num_samples
-        #self.samples = self.gen_samples()
+        self.values = self.gen_values()
 
-    def gen_samples(self):
-        samples = np.random.normal(self.distribution["mean"], self.distribution["var"], self.num_samples)
+    def gen_values(self):
+        values = []
+        for i in range(self.num_data):
+            values = Samples(self.distribution["mean"][i], self.distribution["var"][i], self.num_samples)
+
+        return values
+
+class Samples:
+    def __init__(self, mean, var, num_samples):
+        self.length = mean.shape[0]
+        self.num_samples = num_samples
+        self.samples = self.gen_samples(mean, var)
+        self.mean = self.gen_mean()
+        self.median = self.quantile(0.5)
+
+    def gen_samples(self, mean, var):
+        samples = np.array([])
+        for i in range(self.length):
+            samples = np.append(samples, np.random.normal(mean[i], var[i], self.num_samples))
+
+        samples = samples.reshape(self.length, self.num_samples)
+
         return samples
+
+    def gen_mean(self):
+        mean = np.array([])
+        for i in range(self.length):
+            mean = np.append(mean, np.mean(self.samples[i]))
+        return mean
+
+    def quantile(self, q):
+        return np.quantile(self.samples, q, axis=1)
