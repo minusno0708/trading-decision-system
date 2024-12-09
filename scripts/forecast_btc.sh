a7c7bc3f03cdc98e452272d92320cfa3347b71e0
minusno0708 <naoyao1014@gmail.com>
2024-12-10 00:05:56 +0900

    実行スクリプトの引数を追加

--- scripts/forecast_btc.sh
+++ scripts/forecast_btc.sh
@@ -1,15 +1,22 @@
-python forecast_custom.py \
-    --seed 0 \
-    --output_path output/forecast_btc \
-    --exp_name exp_test \
-    --data_path dataset/btc.csv \
-    --index_col timeOpen \
-    --target_cols close \
-    --train_start_year 2017 \
-    --test_start_year 2023 \
-    --prediction_length 30 \
-    --context_length 30 \
-    --epochs 100 \
-    --num_batches 64 \
-    --num_parallel_samples 1000 \
-    --is_pre_scaling True \
+for seed in 0
+do
+    echo "Running seed $seed"
+    python forecast_custom.py \
+        --seed $seed \
+        --output_path output/forecast_btc \
+        --exp_name normal_350_1208 \
+        --data_path dataset/btc.csv \
+        --index_col timeOpen \
+        --target_cols close \
+        --train_start_year 2019 \
+        --test_start_year 2023 \
+        --prediction_length 30 \
+        --context_length 30 \
+        --epochs 350 \
+        --num_batches 64 \
+        --num_parallel_samples 1000 \
+        --is_pre_scaling 1 \
+        --is_model_scaling 0 \
+        --add_time_features 0 \
+        --add_extention_features 0
+done
--- scripts/forecast_exchange.sh
+++ scripts/forecast_exchange.sh
@@ -1,15 +1,22 @@
-python forecast_custom.py \
-    --seed 0 \
-    --output_path output/forecast_jpy \
-    --exp_name exp_test \
-    --data_path dataset/exchange_rate.csv \
-    --index_col date \
-    --target_cols 5 \
-    --train_start_year 1990 \
-    --test_start_year 2015 \
-    --prediction_length 30 \
-    --context_length 30 \
-    --epochs 100 \
-    --num_batches 64 \
-    --num_parallel_samples 1000 \
-    --is_pre_scaling True \
+for seed in 0
+do
+    echo "Running seed $seed"
+    python forecast_custom.py \
+        --seed $seed \
+        --output_path output/forecast_jpy \
+        --exp_name normal_350_1208 \
+        --data_path dataset/usd_jpy.csv \
+        --index_col Date \
+        --target_cols Price \
+        --train_start_year 1972 \
+        --test_start_year 2023 \
+        --prediction_length 30 \
+        --context_length 30 \
+        --epochs 350 \
+        --num_batches 64 \
+        --num_parallel_samples 1000 \
+        --is_pre_scaling 1 \
+        --is_model_scaling 0 \
+        --add_time_features 0 \
+        --add_extention_features 0
+done
