47eae7d7ec63a0e39c56c5e1b6e3911e89c5f46b
minusno0708 <naoyao1014@gmail.com>
2024-12-12 01:20:52 +0900

    データセット出力用のスクリプトを修正

--- dataset_analyze.py
+++ dataset_analyze.py
@@ -10,7 +10,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 
-target = "jpy"
+target = "btc"
+
+test_start_date = datetime.datetime.strptime("2023-12-01", "%Y-%m-%d")
+train_num = 180
+test_num = 30
 
 if target == "btc":
     target_cols = ["close"]
@@ -21,8 +25,6 @@ if target == "btc":
         prediction_length=30,
         context_length=30,
         freq="D",
-        train_start_date=datetime.datetime(2000, 1, 1),
-        test_start_date=datetime.datetime(2023, 1, 1),
         scaler_flag=False
     )
 elif target == "jpy":
@@ -34,33 +36,47 @@ elif target == "jpy":
         prediction_length=30,
         context_length=30,
         freq="D",
-        train_start_date=datetime.datetime(1972, 1, 1),
-        test_start_date=datetime.datetime(2023, 1, 1),
         scaler_flag=False
     )
+
+data_loader.update_date_by_index(test_start_date, train_num, test_num)
 
 data_array = np.array([])
 
 #data_array = np.append(data_array, data_loader.train[target_cols[0]].values)
 data_array = np.append(data_array, data_loader.test[target_cols[0]].values[30:])
 
-print("ave", np.mean(data_array))
-print("var", np.var(data_array))
-print("std", np.std(data_array))
-print("max", np.max(data_array))
-print("min", np.min(data_array))
+print("平均:", np.mean(data_array).round(2))
+#print("var", np.var(data_array))
+print("標準偏差", np.std(data_array).round(2))
+print("最高値", np.max(data_array).round(2))
+print("最低値", np.min(data_array).round(2))
 
 print("len", len(data_array))
 
 fig, ax = plt.subplots()
 
-#ax.plot(data_loader.train.index, data_loader.train[target_cols[0]].values, label="train", color="blue")
-ax.plot(data_loader.test.index[30:], data_loader.test[target_cols[0]].values[30:], label="test", color="blue")
+ax.plot(data_loader.test.index[:30], data_loader.test[target_cols[0]].values[:30], label="input", color="red")
+ax.plot(data_loader.test.index[30:], data_loader.test[target_cols[0]].values[30:], label="target", color="blue")
 
-#ax.legend()
+ax.legend()
 ax.set_xlabel("Date")
-#ax.set_ylabel("Price($)")
+ax.set_ylabel("Price($)")
 
-ax.xaxis.set_major_locator(mdates.YearLocator(1))
+#ax.xaxis.set_major_locator(mdates.YearLocator(1))
+ax.xaxis.set_major_locator(mdates.MonthLocator())
 
-fig.savefig("output.png")
+fig.savefig("output_test.png")
+plt.close(fig)
+
+fig, ax = plt.subplots()
+
+ax.plot(data_loader.train.index, data_loader.train[target_cols[0]].values, label="train", color="red")
+
+ax.legend()
+ax.set_xlabel("Date")
+ax.set_ylabel("Price($)")
+
+ax.xaxis.set_major_locator(mdates.MonthLocator(interval=1))
+
+fig.savefig("output_train.png")
