c9a9e5145ddd325ec84d46f44144e02307046141
minusno0708 <naoyao1014@gmail.com>
2024-12-17 17:03:48 +0900

    スクリプトの条件を更新

--- scripts/forecast_weather.sh
+++ scripts/forecast_weather.sh
@@ -1,23 +1,23 @@
 for seed in 0
 do
-    for start_date in "2023-01-02"
+    for start_date in "2020-11-06"
     do
         echo "Running seed $seed"
         python forecast_custom.py \
             --seed $seed \
-            --output_path output/forecast_jpy \
-            --exp_name point_forecast_1210 \
-            --data_path dataset/usd_jpy.csv \
-            --index_col Date \
-            --target_cols Price \
+            --output_path output/forecast_wth \
+            --exp_name point_forecast_1214 \
+            --data_path dataset/weather.csv \
+            --index_col date \
+            --target_cols "T (degC)" \
             --test_start_date ${start_date} \
-            --train_data_length 3000 \
-            --test_data_length 360 \
-            --val_data_length 360 \
+            --train_data_length 36600 \
+            --test_data_length 7904 \
+            --val_data_length 7905 \
             --split_type index \
             --prediction_length 1 \
             --context_length 30 \
-            --epochs 300 \
+            --epochs 1 \
             --num_batches 64 \
             --num_parallel_samples 1000 \
             --is_pre_scaling 1 \
