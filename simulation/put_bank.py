4ac16ff0b19f0e5f5a28a1bfc44b9e55d576196d
minusno0708 <naoyao1014@gmail.com>
2024-12-31 00:33:36 +0900

    シミュレーションプログラムを修正

--- simulation/put_bank.py
+++ simulation/put_bank.py
@@ -14,8 +14,14 @@ import sys
 sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 
 from data_provider.data_loader import DataLoader
-from model.DeepAR import MxModel as Model
-from strutegy import *
+from data_provider.custom_data_provider import CustomDataProvider
+
+from model.custom import Model
+
+from simulation.strutegy import *
+from simulation.assets import *
+
+from args_parser import parse_args
 
 seed = 0
 
@@ -86,7 +92,31 @@ class TradeRate:
     def sell(self, target_asset):
         return self.rates[self.target_assets.index(target_asset)]
 
-def main():
+def main(
+    seed,
+    output_path,
+    exp_name,
+    data_path,
+    index_col,
+    target_cols,
+    train_start_date,
+    train_end_date,
+    test_start_date,
+    test_end_date,
+    train_data_length,
+    test_data_length,
+    val_data_length,
+    split_type,
+    prediction_length,
+    context_length,
+    epochs,
+    num_batches,
+    num_parallel_samples,
+    is_pre_scaling,
+    is_model_scaling,
+    add_time_features,
+    add_extention_features
+):
     actions_result = {
         "buy_true": 0,
         "buy_false": 0,
@@ -97,7 +127,11 @@ def main():
     base_asset = "dollar"
     target_assets = ["btc"]
 
+    start_dollar = 0
+
     bank = Bank()
+    bank.put(start_dollar)
+
     btc_hold = 0
     btc_shorted = 0
 
@@ -105,51 +139,74 @@ def main():
 
     rate = TradeRate(base_asset, target_assets)
 
-    data_loader = DataLoader(output_length)
-    _, raw_data = data_loader.load("btc.csv", False, datetime.datetime(2010, 1, 1), datetime.datetime(2023, 1, 1))
-    train_data, test_data = data_loader.load(f"{target_assets[0]}.csv", True, datetime.datetime(2010, 1, 1), datetime.datetime(2023, 1, 1))
+    data_loader = CustomDataProvider(
+        file_path=data_path,
+        index_col=index_col,
+        target_cols=target_cols,
+        prediction_length=prediction_length,
+        context_length=context_length,
+        freq="D",
+        scaler_flag=is_pre_scaling,
+    )
 
-    model = Model(input_length, output_length)
+    if split_type == "datetime":
+        data_loader.update_date(train_start_date, train_end_date, test_start_date, test_end_date)
+    elif split_type == "index":
+        data_loader.update_date_by_index(test_start_date, train_data_length, test_data_length, val_data_length)
+
+    model = Model(
+        context_length=context_length,
+        prediction_length=prediction_length,
+        freq="D",
+        epochs=epochs,
+        num_parallel_samples=num_parallel_samples,
+        target_dim=len(target_cols),
+        is_scaling=is_model_scaling,
+        add_time_features=add_time_features,
+        add_extention_features=add_extention_features,
+    )
+
     if train_flag:
-        model.train(train_data)
-        model.save(f"{output_dir}/models/{target_assets[0]}_model")
+        train_loss, val_loss, minimal_val_loss = model.train(data_loader.train_dataset(batch_size=num_batches, is_shuffle=False), data_loader.val_dataset(batch_size=1, is_shuffle=False))
     else:
-        model.load(f"{output_dir}/models/{target_assets[0]}_model")
+        model.load()
 
-    for d in range(0, len(raw_data) - input_length - output_length):
-        target_scaled_data = test_data.iloc[range(d, input_length + d), [0]]
-        target_raw_data = raw_data.iloc[range(d, input_length + d), [0]]
-        tommorow_data = raw_data.iloc[range(input_length + d, input_length + d + 1), [0]]
+    strutegy = Strategy(decision_method)
+
+    for i, (start_date, input_x, target_x, time_features, extention_features) in enumerate(data_loader.test_dataset(batch_size=1, is_shuffle=False)):
+        # 価格を予測
+        forecasts, loss = model.make_evaluation_predictions(input_x, target_x, time_features, extention_features)
+        
+        if is_pre_scaling:
+            input_x_inverse = []
+            target_x_inverse = []
+
+            for c, col in enumerate(target_cols):
+                forecasts[c].inverse_transform(data_loader.scaler[col])
+                input_x_inverse.append(data_loader.inverse_transform(input_x[:,:,c].detach().numpy().squeeze(0), col).squeeze(0))
+                target_x_inverse.append(data_loader.inverse_transform(target_x[:,:,c].detach().numpy().squeeze(0), col).squeeze(0))
+
+            input_x = np.array(input_x_inverse)
+            target_x = np.array(target_x_inverse)
+        else:
+            input_x = input_x.squeeze(0).permute(1, 0).detach().numpy()
+            target_x = target_x.squeeze(0).permute(1, 0).detach().numpy()
+
+        current_rate = input_x[0][-1]
 
         # 仮想通貨の価格を更新
         rate.update([
-            target_raw_data.values[-1][0]
+            current_rate,
         ])
 
-        # 明日以降の価格を予測
-        forecasts = model.forecast(target_scaled_data)
-
         # 取引開始
-        history_values = data_loader.inverse_transform(target_scaled_data.values.reshape(-1))[0]
-        forecast_values = data_loader.inverse_transform(forecasts[0].mean)[0]
+        history_values = input_x[0]
+        future_values = target_x[0]
+        forecast_values = forecasts[0].mean
 
-        if decision_method == "diff_next_mean":
-            action = diff_next_mean(history_values, forecast_values)
-        elif decision_method == "random":
-            action = random_decision()
-        elif decision_method == "all_true":
-            action = all_true(history_values, tommorow_data.values[0][0])
-        elif decision_method == "all_false":
-            action = all_false(history_values, tommorow_data.values[0][0])
-        elif decision_method == "all_buy":
-            action = "buy"
-        elif decision_method == "all_sell":
-            action = "sell"
-        else:
-            print("method not found")
-            return
+        action = strutegy.decide_action(history_values, forecast_values, future_values)
 
-        current_date = target_raw_data.index[-1]
+        current_date = history_values[-1]
 
         # 保持しているビットコインを売却
         bank.put(btc_hold * rate.sell("btc"))
@@ -175,16 +232,16 @@ def main():
 
         # 行動とその結果を記録
         if action == "buy":
-            if history_values[-1] < tommorow_data.values[0][0]:
+            if history_values[-1] < future_values[0]:
                 actions_result["buy_true"] += 1
-            elif history_values[-1] > tommorow_data.values[0][0]:
+            elif history_values[-1] > future_values[0]:
                 actions_result["buy_false"] += 1
             else:
                 print("Error")
         elif action == "sell":
-            if history_values[-1] > tommorow_data.values[0][0]:
+            if history_values[-1] > future_values[0]:
                 actions_result["sell_true"] += 1
-            elif history_values[-1] < tommorow_data.values[0][0]:
+            elif history_values[-1] < future_values[0]:
                 actions_result["sell_false"] += 1
             else:
                 print("Error")
@@ -204,5 +261,37 @@ def main():
 
 if __name__ == '__main__':
     print("desicion method: ", decision_method)
-    main()
+    args = parse_args()
+
+    seed = args.seed
+
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+
+    main(
+        seed=seed,
+        output_path=args.output_path,
+        exp_name=args.exp_name,
+        data_path=args.data_path,
+        index_col=args.index_col,
+        target_cols=args.target_cols.split(","),
+        train_start_date=args.train_start_date,
+        train_end_date=args.train_end_date,
+        test_start_date=args.test_start_date,
+        test_end_date=args.test_end_date,
+        train_data_length=args.train_data_length,
+        test_data_length=args.test_data_length,
+        val_data_length=args.val_data_length,
+        split_type=args.split_type,
+        prediction_length=args.prediction_length,
+        context_length=args.context_length,
+        epochs=args.epochs,
+        num_batches=args.num_batches,
+        num_parallel_samples=args.num_parallel_samples,
+        is_pre_scaling=args.is_pre_scaling,
+        is_model_scaling=args.is_model_scaling,
+        add_time_features=args.add_time_features,
+        add_extention_features=args.add_extention_features,
+    )
     
